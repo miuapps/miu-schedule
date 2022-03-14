66bab1e602fc4630e3c2bf9df77aa5c9418344af
Emran <emran@Emrans-MacBook-Pro.local>
2022-03-14 14:26:02 -0500

    Initial commit

---
+++ src/main/java/com/miuapps/miuschedule/MiuScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.miuapps.miuschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiuScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiuScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miuapps/miuschedule/MiuScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miuapps.miuschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiuScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
